cc5b1c6ff82204b2e050501127cf82abdde33a16
Adam Bieszk <adambieszk6@gmail.com>
2026-02-17 01:28:18 +0100

    fix: added conditional sprite for haemotoxia

--- main.lua
+++ main.lua
@@ -2,20 +2,20 @@ local modName = "Last Judgement Boss Icons"
 LJBossIcons = RegisterMod(modName, 1)
 
 -- #region check of required mods being installed
-    --#region Last Judgement
-    if not LastJudgement then
-        print("[" .. modName .. "] This mod requires Last Judgement!")
-        Isaac.DebugString("[" .. modName .. "] This mod requires Last Judgement!")
-        return
-    end
-    --#endregion
-    --#region Enhanced Boss Bars
-    if not HPBars then
-        print("[" .. modName .. "] This mod requires Enhanced Boss Bars!")
-        Isaac.DebugString("[" .. modName .. "] This mod requires Enhanced Boss Bars!")
-        return
-    end
-    --#endregion
+--#region Last Judgement
+if not LastJudgement then
+    print("[" .. modName .. "] This mod requires Last Judgement!")
+    Isaac.DebugString("[" .. modName .. "] This mod requires Last Judgement!")
+    return
+end
+--#endregion
+--#region Enhanced Boss Bars
+if not HPBars then
+    print("[" .. modName .. "] This mod requires Enhanced Boss Bars!")
+    Isaac.DebugString("[" .. modName .. "] This mod requires Enhanced Boss Bars!")
+    return
+end
+--#endregion
 -- #endregion
 
 if HPBars then
@@ -24,61 +24,69 @@ if HPBars then
         --#region Mama Plum
         ["908.743"] = {
             sprite = "gfx/ui/bosshp_icons/minibosses/mamaplum/mamaplum.png",
-            offset = Vector(-10,0)
+            offset = Vector(-10, 0)
         },
         --#endregion
-    --#endregion
-    --#region Mortis
+        --#endregion
+        --#region Mortis
         --#region AIDS
         ["744.2"] = {
             sprite = "gfx/ui/bosshp_icons/minibosses/aids/aids.png",
-            offset = Vector(-10,0)
+            offset = Vector(-10, 0)
         },
         --#endregion
         --#region Cadavra
         ["743.1000"] = {
             sprite = "gfx/ui/bosshp_icons/mortis/cadavra/cadavra.png",
-            offset = Vector(-8,0)
+            offset = Vector(-8, 0)
         },
         --#endregion
         --#region Chubs
         ["743.1001"] = {
             sprite = "gfx/ui/bosshp_icons/mortis/cadavra/chubs.png",
-            offset = Vector(-8,0)
+            offset = Vector(-8, 0)
         },
         --#endregion
         --#region Nibs
         ["743.1002"] = {
             sprite = "gfx/ui/bosshp_icons/mortis/cadavra/nibs.png",
-            offset = Vector(-4,0)
+            offset = Vector(-4, 0)
         },
         --#endregion
         --#region Pinky
         ["743.1010"] = {
             sprite = "gfx/ui/bosshp_icons/mortis/pinky/pinky.png",
-            offset = Vector(-10,0)
+            offset = Vector(-10, 0)
         },
         --#endregion
         --#region Haemotoxia
         ["743.1020"] = {
             sprite = "gfx/ui/bosshp_icons/mortis/haemotoxia/haemotoxia.png",
-            offset = Vector(-7,0)
+            conditionalSprites = {
+                { "isHPSmallerPercent", "gfx/ui/bosshp_icons/mortis/haemotoxia/haemotoxia_phase2.png", { 40 } }
+            },
+            offset = Vector(-7, 0)
         },
         --#endregion
-        --#region Mother (phase 1)
-        ["912.0"] = {
-    		sprite = "gfx/ui/bosshp_icons/final/mother/mother.png",
-    		barStyle = "Mother",
-    		offset = Vector(-10, 0)
-    	},
-    	--#endregion
-    	--#region Mother (phase 2)
-        ["912.10"] = {
-            sprite = "gfx/ui/bosshp_icons/final/mother/mother_phase2.png",
-            barStyle = "Mother",
-            offset = Vector(-8, 0)
-	   }
-	   --#endregion
-	}
+    }
     --#endregion
+
+    if LastJudgement.STAGE.Mortis:IsStage() then
+        HPBars.BossDefinitions = {
+            --#region Mother (phase 1)
+            ["912.0"] = {
+                sprite = "gfx/ui/bosshp_icons/final/mother/mother.png",
+                barStyle = "Mother",
+                offset = Vector(-10, 0)
+            },
+            --#endregion
+            --#region Mother (phase 2)
+            ["912.10"] = {
+                sprite = "gfx/ui/bosshp_icons/final/mother/mother_phase2.png",
+                barStyle = "Mother",
+                offset = Vector(-8, 0)
+            }
+            --#endregion
+        }
+    end
 end
